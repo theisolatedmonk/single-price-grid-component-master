45a9d0ff0fe760403b25c127edff6c008df6793c
theisolatedmonk <96rahulseth@gmail.com>
2023-11-01 10:41:17 +0530

    color implimented

--- app/page.js
+++ app/page.js
@@ -2,35 +2,42 @@ import Image from 'next/image'
 
 export default function Home () {
   return (
-    <div className='flex flex-col w-full p-4'>
-      <div className='flex flex-col p-2'>
-        <p className='font-medium text-xl '>Join our community </p>
-        <p className='font-medium text-lg'>
+    <div className='flex flex-col w-full px-4 justify-center items-center min-h-screen bg-GrayishBlue'>
+      <div className='flex flex-col p-6 gap-5 bg-white rounded-t-md sm:w-[50%]'>
+        <p className='font-medium text-xl text-Cyan'>Join our community </p>
+        <p className='font-medium text-sm text-BrightYellow'>
           30-day, hassle-free money back guarantee
         </p>
-        <p className='text-sm'>
+        <p className='text-sm text-GrayishBlue'>
           Gain access to our full library of tutorials along with expert code
           reviews. Perfect for any developers who are serious about honing their
           skills.
         </p>
       </div>
-      <div className='flex flex-col p-2'>
-        <p className='font-medium text-xl '>Monthly Subscription </p>
-        <p className='font-medium text-3xl '>
-          $29 <span className='font-normal text-sm'>per month</span>
-        </p>
-        <p className='font-medium text-lg'>
-          Full access for less than $1 a day
-        </p>
-        <button className='w-full p-2'>Sign Up</button>
-      </div>
-      <div className='flex flex-col p-2'>
-        <p className='font-medium text-xl '>Why Us</p>
-        <p className='text-sm'>
-          Tutorials by industry experts Peer & expert code review Coding
-          exercises Access to our GitHub repos Community forum Flashcard decks
-          New videos every week
-        </p>
+      <div className='flex flex-col sm:flex-row sm:w-[50%]  '>
+        <div className='flex flex-col  bg-Cyan text-white sm:w-[50%] p-5 gap-4   sm:rounded-bl-md'>
+          <p className='font-medium text-lg '>Monthly Subscription </p>
+          <p className='font-medium text-2xl '>
+            $29{' '}
+            <span className='font-normal text-sm text-GrayishBlue'>
+              per month
+            </span>
+          </p>
+          <p className='font-medium text-sm  text-LightGray'>
+            Full access for less than $1 a day
+          </p>
+          <button className='w-full p-3 mt-2 shadow-lg bg-BrightYellow rounded-md text-sm'>
+            Sign Up
+          </button>
+        </div>
+        <div className='flex flex-col  sm:w-[50%] bg-Cyan bg-opacity-50 text-white p-6 justify-start rounded-b-md sm:rounded-b-none sm:rounded-br-md'>
+          <p className='font-medium text-lg gap-4  '>Why Us</p>
+          <p className='text-sm text-LightGray text-left'>
+            Tutorials by industry experts Peer & expert code review Coding
+            exercises Access to our GitHub repos Community forum Flashcard decks
+            New videos every week
+          </p>
+        </div>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,22 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
-    },
+      colors: {
+        Cyan: 'hsl(179, 62%, 43%)',
+        BrightYellow: 'hsl(71, 73%, 54%)',
+        LightGray: 'hsl(204, 43%, 93%)',
+        GrayishBlue: 'hsl(218, 22%, 67%)'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
